918e4bd6fdd2cd27b0e8d2f9b2d53ef1a869d5b6
Jaskarn <jaskarn15@gmail.com>
2022-04-18 21:42:10 +0000

    adding url endpoint

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-const urlDatabases = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -10,6 +10,10 @@ const urlDatabases = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
